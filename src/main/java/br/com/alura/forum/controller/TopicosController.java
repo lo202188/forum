ef3e54d6418866822fbbbdb48ec87e85835e8630
lorenatomagnini <lorena.tomagnini@thoughtworks.com>
2021-06-16 11:58:22 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,25 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class TopicosController {
+	
+	@RequestMapping("/topicos")
+	public List <TopicoDto> lista(){
+		
+		Topico topico = new Topico("Dúvida","Dúvida com Spring", new Curso("Spring","Programação"));
+		
+		return TopicoDto.converter(Arrays.asList(topico,topico,topico));
+	}
+}
